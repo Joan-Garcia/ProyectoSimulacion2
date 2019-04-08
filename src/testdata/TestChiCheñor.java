33384e11ad450d94963f6583b1fd03b332006a72
HunterXB <HunterXB@DESKTOP-6DHAUGO>
2019-04-07 21:37:35 -0500

    Implementacion del metodo chi ...

---
+++ src/testdata/TestChiCheñor.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testdata;
+
+import methods.methodChi;
+
+public class TestChiCheñor {
+   
+    public static void main(String[] args) {
+        new methodChi().corrida();
+    }
+}
